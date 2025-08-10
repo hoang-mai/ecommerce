c71b553a0a912aa7a0395d5da9311bcb44e66f06
HoangMA <maianhhoang31072003@gmail.com>
2025-08-10 10:25:44 +0700

    authen

--- user-service/src/main/java/com/example/edu/school/user/config/GrpcConfig.java
+++ user-service/src/main/java/com/example/edu/school/user/config/GrpcConfig.java
@@ -1,14 +1,17 @@
 package com.example.edu.school.user.config;
 
 import com.example.edu.school.auth.AccountServiceGrpc;
+import io.grpc.ManagedChannel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.grpc.client.ChannelBuilderOptions;
 import org.springframework.grpc.client.GrpcChannelFactory;
 import org.springframework.grpc.client.interceptor.security.BearerTokenAuthenticationInterceptor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
 
@@ -16,12 +19,17 @@ import java.util.List;
 public class GrpcConfig {
 
     @Bean
-    AccountServiceGrpc.AccountServiceBlockingStub stub(GrpcChannelFactory channels) {
-        return AccountServiceGrpc.newBlockingStub(channels.createChannel("auth-service",
-                ChannelBuilderOptions.defaults().withInterceptors(List.of(new BearerTokenAuthenticationInterceptor(() -> {
-                    JwtAuthenticationToken authentication =(JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-                    System.out.println("Authentication: " + authentication + ", Token: " + authentication.getToken().getTokenValue());
-                    return authentication.getToken().getTokenValue();
-                })))));
+    @RequestScope
+    public AccountServiceGrpc.AccountServiceBlockingStub stub(GrpcChannelFactory channels) {
+        ChannelBuilderOptions channelBuilderOptions = ChannelBuilderOptions.defaults().withInterceptors(
+                List.of(new BearerTokenAuthenticationInterceptor(() -> {
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    if (authentication instanceof JwtAuthenticationToken jwtAuthenticationToken) {
+                        return jwtAuthenticationToken.getToken().getTokenValue();
+                    }
+                    return null;
+                })));
+        ManagedChannel managedChannel = channels.createChannel("auth-service", channelBuilderOptions);
+        return AccountServiceGrpc.newBlockingStub(managedChannel);
     }
 }
