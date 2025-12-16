2c6e90be7769359c0e51c59f5ac09034cdda0b3a
HoangMA <maianhhoang31072003@gmail.com>
2025-12-16 17:28:20 +0700

    edit for deploy

--- saga-service/src/main/java/com/ecommerce/saga/config/GrpcConfig.java
+++ saga-service/src/main/java/com/ecommerce/saga/config/GrpcConfig.java
@@ -2,10 +2,6 @@ package com.ecommerce.saga.config;
 
 import com.ecommerce.auth.AccountServiceGrpc;
 import com.ecommerce.user.UserServiceGrpc;
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,29 +11,14 @@ import org.springframework.grpc.client.GrpcChannelFactory;
 @RequiredArgsConstructor
 public class GrpcConfig {
 
-    private final EurekaClient eurekaClient;
-
-    private ManagedChannel createChannel(String serviceName){
-        InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka(serviceName.toUpperCase(), false);
-        String address = instanceInfo.getIPAddr();
-        int port = instanceInfo.getMetadata().get("grpc-port") != null ?
-                Integer.parseInt(instanceInfo.getMetadata().get("grpc-port")) :
-                instanceInfo.getPort();
-        return ManagedChannelBuilder.forAddress(address, port)
-                .usePlaintext()
-                .build();
-    }
-
-
-
     @Bean
-    public AccountServiceGrpc.AccountServiceBlockingStub stubAuthService() {
-        return AccountServiceGrpc.newBlockingStub(createChannel("auth-service"));
+    public AccountServiceGrpc.AccountServiceBlockingStub stubAuthService(GrpcChannelFactory channels) {
+        return AccountServiceGrpc.newBlockingStub(channels.createChannel("auth-service"));
     }
 
     @Bean
-    public UserServiceGrpc.UserServiceBlockingStub stubUserService() {
-        return UserServiceGrpc.newBlockingStub(createChannel("user-service"));
+    public UserServiceGrpc.UserServiceBlockingStub stubUserService(GrpcChannelFactory channels) {
+        return UserServiceGrpc.newBlockingStub(channels.createChannel("user-service"));
     }
 
 }
